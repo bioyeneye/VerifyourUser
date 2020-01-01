f5268efdb8f6143f61dfeec5489a6ba41f147f65
oyeneyebolaji <oyeneyebolaji@OYENEYEBOLA5354>
2020-01-01 18:42:52 +0100

    Define user abstract implementation

---
+++ VerifyourUser.Domain/Core/UserManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VerifyourUser.DataAccess.Context;
+using VerifyourUser.DataAccess.Entities;
+using VerifyourUser.Domain.Core.Abstract;
+using VerifyourUser.Domain.ResponseModel;
+
+namespace VerifyourUser.Domain.Core
+{
+    public class UserManager : IUserManager
+    {
+        private VerifyourUserDbContext _dbContext;
+        private ILogger<UserManager> _logger;
+
+        public UserManager(VerifyourUserDbContext dbContext, ILogger<UserManager> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public Task<GenericResponseModel<UserCreationResponse>> CreateUser(string audienceId, string firstname, string lastname, string phonenumber, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponseModel<UserTokenGenerationResponse>> GenerateUserToken(string userId, string token, UserVerificationTokenType userVerificationTokenType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponseModel<UserTokenGenerationResponse>> GetToken(string userId, UserVerificationTokenType userVerificationTokenType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponseModel<UserVerificationResponse>> VerifyUser(string userId, string token, UserVerificationTokenType userVerificationTokenType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
